0674d82c4484f25065446bcb9e7d1335bdd9f091
DoubleShx <shaxzodshamaxmudov@gmail.com>
2022-09-24 20:44:33 +0500

    connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,15 +12,20 @@ import { AppController } from "./app.controller";
             envFilePath: `.${process.env.NODE_ENV}.env`
         }),
         SequelizeModule.forRoot({
-        dialect: 'postgres',
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        models: [User],
-        autoLoadModels: true
-    }), UsersModule, AuthModule],
+            dialect: 'postgres',
+            "dialectOptions": {
+                "ssl": true
+            },
+            logging: false,
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [User],
+            autoLoadModels: true,
+            synchronize: true
+        }), UsersModule, AuthModule],
     controllers: [AppController],
     providers: []
 })
